3b905982fed1546c01857ba34bc85a7f63448afd
Rob Sheldrick <lebaguettemuncherer@gmail.com>
2025-01-13 14:10:22 +0000

    feature: responsive youtube player

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import { signIn, signOut, useSession } from "next-auth/react";
-import React from "react";
+import React, { useEffect } from "react";
 import { api } from "~/utils/api";
 import { extractVideoId, fmtMSS } from "packages/helpers";
 import YouTube, { type YouTubeEvent, type YouTubeProps } from 'react-youtube'
 import type { YouTubePlayer } from 'youtube-player/dist/types'
 import ReactSlider from "react-slider";
+import debounce from 'lodash.debounce'
 export default function Home() {
   const [sliderValues, setSliderValues] = React.useState([0, 100])
   // eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents
@@ -42,13 +43,57 @@ export default function Home() {
     }
   }
 
-  void React.useMemo(async () => {
-    await snapToLoop()
+  const voidSnapToLoop = () => {
+    void snapToLoop()
+  }
+  React.useMemo(() => {
+    voidSnapToLoop()
   }, [currentTime, snapToLoop])
 
+
+
+  const setSize = async (width: number, height: number) => {
+    if (playerRef.current) {
+      await playerRef.current.setSize(width, height)
+    }
+  }
+  const voidSetSize = (width: number, height: number) => {
+    void setSize(width, height)
+  }
+
+  const handleResize = () => {
+    console.log('...resizing')
+    const width = window.innerWidth
+    let playerWidth = width / 1.5
+
+    switch (true) {
+      case width > 1280:
+        playerWidth = width / 2
+        break
+      case width > 768:
+        playerWidth = width / 1.5
+        break
+      default:
+        playerWidth = width - 40
+        break
+    }
+    const playerHeight = playerWidth * (9 / 16)
+    if (playerRef.current) {
+      voidSetSize(playerWidth, playerHeight)
+    }
+  }
+
+  const debouncedHandleResize = debounce(handleResize, 500)
+
+  useEffect(() => {
+    handleResize()
+    window.addEventListener("resize", debouncedHandleResize)
+  })
+
+
   const playerOpts = React.useState({
-    height: '390',
-    width: '640',
+    height: 390,
+    width: 560,
     // playerVars: {
     //   // https://developers.google.com/youtube/player_parameters
     //   autoplay: 1,
@@ -114,8 +159,8 @@ export default function Home() {
   const thumbClasses = "absolute p-2 rounded-xl cursor-pointer text-white "
 
   return (
-    <div className="bg-slate-700 h-screen">
-      <AuthShowcase />
+    <div className="bg-slate-700 h-screen flex flex-col justify-center pt-10 pb-40">
+      {/* <AuthShowcase /> */}
       <div className="flex flex-col gap-10 items-center pt-5">
         <div className="bg-gray-200 p-5 rounded-lg w-11/12 sm:w-1/2 md:w-1/3">
           <label htmlFor="link" className="block text-sm/6 font-medium text-gray-900">
@@ -134,7 +179,7 @@ export default function Home() {
         </div>
 
         <div className="w-full flex flex-col items-center gap-10">
-          <YouTube className="bg-black p-3 rounded-xl" videoId={videoId} opts={playerOpts[0]} onReady={onPlayerReady} onStateChange={onStateChange} />
+          <YouTube className="select-none bg-gray-600 p-4 rounded-xl" videoId={videoId} opts={playerOpts[0]} onReady={onPlayerReady} onStateChange={onStateChange} />
           <div className="w-full flex justify-center items-center">
             <div className="w-11/12 sm:w-2/3 md:w-1/2 bg-slate-600 p-5 pb-8 rounded-3xl">
               <ReactSlider
@@ -150,7 +195,7 @@ export default function Home() {
                 renderThumb={(props, state) =>
                   <div {...props}>
                     <div className={state.index ? `${thumbClasses + "-bottom-12 -right-5"}` : `${thumbClasses + "bottom-5 -right-5"}`}>
-                      <p>{mssNums[state.valueNow] ?? state.valueNow}</p>
+                      <p className="select-none">{mssNums[state.valueNow] ?? state.valueNow}</p>
                     </div>
                   </div>
                 }
