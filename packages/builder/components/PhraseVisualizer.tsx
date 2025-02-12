4b34a68cdd20a425118005db94035b71c1c22f72
Rob Sheldrick <lebaguettemuncherer@gmail.com>
2025-02-12 16:12:44 +0000

    fix: phrase visualiser button area extended to padding

--- packages/builder/components/PhraseVisualizer.tsx
+++ packages/builder/components/PhraseVisualizer.tsx
@@ -1,29 +1,43 @@
-import { useTuneBuilderStore } from "../store"
-import { useLooperStore } from "packages/looper/store"
-import clsx from 'clsx'
-export const PhraseVisualizer = ({ zoomTrack, isZoomed }: { zoomTrack: (start: number, end: number) => void, isZoomed: boolean }) => {
-    const { setSliderValues } = useLooperStore()
-    const builder = useTuneBuilderStore()
+import { useTuneBuilderStore } from "../store";
+import { useLooperStore } from "packages/looper/store";
+import clsx from "clsx";
+export const PhraseVisualizer = ({
+    zoomTrack,
+    isZoomed,
+}: {
+    zoomTrack: (start: number, end: number) => void;
+    isZoomed: boolean;
+}) => {
+    const { setSliderValues } = useLooperStore();
+    const builder = useTuneBuilderStore();
 
     return (
         <div className="flex flex-col items-center">
             <ol className="flex select-none flex-wrap">
                 {builder.phrases.map((phrase) => (
-                    <li key={phrase.idx} className=
-                        {clsx(
-                            'border border-black p-2 m-2 text-white cursor-pointer h-9 w-9 overflow-hidden text-center flex justify-center items-center',
-                            { 'bg-green-300': phrase.idx === builder.selectedPhraseIdx }
-                        )}
-                        onClick={() => {
-                            builder.setSelectedPhrase(phrase.idx)
-                            setSliderValues([phrase.startTime, phrase.endTime])
-                            if (!isZoomed) return
-                            zoomTrack(phrase.startTime, phrase.endTime)
-                        }}>
-                        <p>{phrase.idx + 1}</p>
-                    </li>))}
-                {builder.phrases.length === 0 && <div className="border border-black p-2 m-2 text-white">No phrases</div>}
+                    <li key={phrase.idx}>
+                        <button
+                            className={clsx(
+                                "m-2 flex h-9 w-9 cursor-pointer items-center justify-center overflow-hidden border border-black p-2 text-center text-white",
+                                { "bg-green-300": phrase.idx === builder.selectedPhraseIdx },
+                            )}
+                            onClick={() => {
+                                builder.setSelectedPhrase(phrase.idx);
+                                setSliderValues([phrase.startTime, phrase.endTime]);
+                                if (!isZoomed) return;
+                                zoomTrack(phrase.startTime, phrase.endTime);
+                            }}
+                        >
+                            <p>{phrase.idx + 1}</p>
+                        </button>
+                    </li>
+                ))}
+                {builder.phrases.length === 0 && (
+                    <div className="m-2 border border-black p-2 text-white">
+                        No phrases
+                    </div>
+                )}
             </ol>
         </div>
-    )
-}
+    );
+};
